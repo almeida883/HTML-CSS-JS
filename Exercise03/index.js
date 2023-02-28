c26451a17639ed42d6383554ff04d56511bcde47
almeida883 <almeidagamer99@gmail.com>
2023-02-28 04:11:40 +0000

    Update

---
+++ Exercise03/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.box_icon_share_01').click(function() {
+    $('.sharing_box_phone').toggleClass("hidden");
+  });
+
+  $('.sharing_box_phone').click(function() {
+    $('.sharing_box_phone').toggleClass("hidden");
+  });
+});
