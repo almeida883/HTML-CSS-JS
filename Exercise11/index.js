4bb1b89d1a793db3fb7cbfa8d771f3a44b989dad
almeida883 <almeidagamer99@gmail.com>
2023-03-14 22:12:29 +0000

    Update

--- Exercise11/index.js
+++ Exercise11/index.js
@@ -1,44 +1,88 @@
 $(function() {
-  $('#tipCustom').hide();
 
-  $('input[name="tip"]').change(function() {
-      if ($(this).val() === 'custom') {
-          $('#customTip').show();
-      } else {
-          $('#customTip').hide();
-      }
-      calculate();
-  });
+    $('#bill, #custom, #people').on('input', function() {
+        if ($.isNumeric($(this).val())) {
+          $(this).addClass('border_cyan');
+          $('#button_reset').removeClass("button_empty");
+        } else {
+          $(this).removeClass('border_cyan');
+        }
+      });
 
-  $('#bill, #people').keyup(function() {
-      calculate();
-  });
+      $('#bill, #custom, #people').on('input', function() {
+        if (($('#bill').val() || $('#custom').val()) && ($('#people').val() === '' || isNaN($('#people').val()))) {
+          $('#people').addClass('error');
+          $('.error_msg').css("display", "block");
+        } else {
+          $('#people').removeClass('error');
+          $('.error_msg').css("display", "none");
+        }
+      });
 
-  $('#customTip').keyup(function() {
-      calculate();
-  });
+     $('#people').on('input', function() {
+        $('#people').removeClass('error');
+        $('.error_msg').css("display", "none");
+    });
+      
+    var tipPercent = 0.10;
 
-  $('form').on('reset', function() {
-      $('#customTip').hide();
-      $('#tip5').prop('checked', true);
-      $('#bill, #people').val('');
-      $('#tipAmount, #totalAmount').text('');
-  });
+    $('button').click(function() {
+        $('button').removeClass('active');
+        $(this).addClass('active');
+        tipPercent = parseFloat($(this).attr('id')) / 100; // atualiza a variável tipPercent com o valor do botão clicado
+        $('#custom').val('');
+        $('#custom').removeClass('border_cyan');
+        calculate();
+      });      
 
-  function calculate() {
-      var bill = parseFloat($('#bill').val());
-      var tipPercent;
-      if ($('input[name="tip"]:checked').val() === 'custom') {
-          tipPercent = parseFloat($('#customTip').val()) / 100;
-      } else {
-          tipPercent = parseFloat($('input[name="tip"]:checked').val());
-      }
-      var numPeople = parseInt($('#people').val());
+      $('#custom').keyup(function() {
+        // Verifica se o campo personalizado foi preenchido
+        if ($(this).val()) {
+            // Remove a classe 'active' do botão selecionado
+            $('button').removeClass('active');
+            // Atualiza a variável tipPercent com o valor do campo personalizado
+            tipPercent = parseFloat($(this).val()) / 100;
+        } else {
+            tipPercent = 0;
+        }
+        // Calcula a gorjeta
+        calculate();
+    });
 
-      var tipAmount = (bill * tipPercent) / numPeople;
-      var totalAmount = ((bill + (bill * tipPercent)) / numPeople);
+    // Monitora as teclas que são pressionadas no campo de entrada da conta e número de pessoas
+    $('#bill, #people').keyup(function() {
+        // Calcula a gorjeta
+        calculate();
+    });
 
-      $('#tipAmount').text('$' + tipAmount.toFixed(2));
-      $('#totalAmount').text('$' + totalAmount.toFixed(2));
-  }
+    // Reseta o formulário
+    $('form').on('reset', function() {
+        tipPercent = 0.10;
+        // Limpa os campos de entrada da conta e número de pessoas
+        $('#bill, #people, #custom').val('');
+        // Limpa as áreas de exibição de resultado
+        $('#tipAmount, #totalAmount').text('$0.00');
+        $('#people').removeClass('error');
+        $('.error_msg').css("display", "none");
+        $('#bill, #custom, #people').removeClass('border_cyan');
+        $('#button_reset').addClass("button_empty");
+    });
+
+    // Função que calcula a gorjeta
+    function calculate() {
+        // Pega o valor da conta e transforma em um número
+        var bill = parseFloat($('#bill').val());
+        // Pega o número de pessoas e transforma em um número
+        var numPeople = parseInt($('#people').val());
+
+        // Calcula a quantia de gorjeta por pessoa
+        var tipAmount = (bill * tipPercent) / numPeople;
+        // Calcula o valor total por pessoa (conta + gorjeta)
+        var totalAmount = (bill + (bill * tipPercent)) / numPeople;
+
+        // Exibe a quantia de gorjeta por pessoa com duas casas decimais e o símbolo '$'
+        $('#tipAmount').text('$' + tipAmount.toFixed(2));
+        // Exibe o valor total por pessoa com duas casas decimais e o símbolo '$'
+        $('#totalAmount').text('$' + totalAmount.toFixed(2));
+    }
 });
