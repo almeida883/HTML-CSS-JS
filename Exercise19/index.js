888411bd9f0a7bc06b99c242a3bf4891b70fda56
almeida883 <almeidagamer99@gmail.com>
2023-04-07 04:58:01 +0100

    Update

--- Exercise19/index.js
+++ Exercise19/index.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.error_message,.error_day,.error_month,.error_year').hide();
+  $('.error_message_day,.error_message_month,.error_message_year,.error_day,.error_month,.error_year').hide();
  
   var anoAtual = new Date().getFullYear();
   
@@ -20,29 +20,103 @@ $(function() {
       var mesAtual = dataAtual.getMonth() + 1;
 
       // Verifica se os campos estão preenchidos
+
+      if (dia === '') {
+        $('.error_message_day').show();
+        $('#dia').addClass('border_color_red');
+        $('.label_text_day').addClass('color_red');
+      }
+      
+      if (mes === '') {
+        $('.error_message_month').show();
+        $('#mes').addClass('border_color_red');
+        $('.label_text_month').addClass('color_red');
+      }
+      
+      if (ano === '') {
+        $('.error_message_year').show();
+        $('#ano').addClass('border_color_red');
+        $('.label_text_year').addClass('color_red');
+      }
+      
       if (dia === '' || mes === '' || ano === '') {
-        $('.error_message').show();
         return false;
       } else {
-        $('.error_message').hide();
+        $('.error_message_day,.error_message_month,.error_message_year').hide();
+        $('#dia,#mes,#ano').removeClass('border_color_red');
+        $('.label_text_day,.label_text_month,.label_text_year').removeClass('color_red');
       }
+      
 
-      // Verifica se o ano, mês e dia são válidos
-      if (ano > anoAtual || ano < 1 || (ano == anoAtual && mes > mesAtual) || (ano == anoAtual && mes == mesAtual && dia > diaAtual) || mes < 1 || mes > 12 || dia < 1 || dia > diasNoMes(mes, ano)) {    
-        $('.error_day,.error_month,.error_year').show();
-        return false;
-      } else {
-        $('.error_day,.error_month,.error_year').hide();
-      }
+// Verifica se o ano, mês e dia são válidos
+if (ano > anoAtual || ano < 1) {
+  $('.error_year').show();
+  $('#ano').addClass('border_color_red');
+  $('.label_text_year').addClass('color_red');
+  return false;
+} else {
+  $('.error_year').hide();
+  $('#ano').removeClass('border_color_red');
+  $('.label_text_year').removeClass('color_red');
+}
 
-      // Verifica se a data é inválida
-      var data = new Date(ano, mes - 1, dia);
-      if (data.getFullYear() != ano || data.getMonth() + 1 != mes || data.getDate() != dia) {
-        $('.error_day,.error_month,.error_year').show();
-        return false;
-      } else {
-        $('.error_day,.error_month,.error_year').hide();
-      }
+if (ano == anoAtual && mes > mesAtual) {
+  $('.error_month').show();
+  $('#mes').addClass('border_color_red');
+  $('.label_text_month').addClass('color_red');
+  return false;
+} else {
+  $('.error_month').hide();
+  $('#mes').removeClass('border_color_red');
+  $('.label_text_month').removeClass('color_red');
+}
+
+if (ano == anoAtual && mes == mesAtual && dia > diaAtual) {
+  $('.error_day').show();
+  $('#dia').addClass('border_color_red');
+  $('.label_text_day').addClass('color_red');
+  return false;
+} else {
+  $('.error_day').hide();
+  $('#dia').removeClass('border_color_red');
+  $('.label_text_day').removeClass('color_red');
+}
+
+if (mes < 1 || mes > 12) {
+  $('.error_month').show();
+  $('#mes').addClass('border_color_red');
+  $('.label_text_month').addClass('color_red');
+  return false;
+} else {
+  $('.error_month').hide();
+  $('#mes').removeClass('border_color_red');
+  $('.label_text_month').removeClass('color_red');
+}
+
+if (dia < 1 || dia > diasNoMes(mes, ano)) {    
+  $('.error_day').show();
+  $('#dia').addClass('border_color_red');
+  $('.label_text_day').addClass('color_red');
+  return false;
+} else {
+  $('.error_day').hide();
+  $('#dia').removeClass('border_color_red');
+  $('.label_text_day').removeClass('color_red');
+}
+
+// Verifica se a data é inválida
+var data = new Date(ano, mes - 1, dia);
+if (data.getFullYear() != ano || data.getMonth() + 1 != mes || data.getDate() != dia) {
+  $('.error_day,.error_month,.error_year').show();
+  $('#dia,#mes,#ano').addClass('border_color_red');
+  $('.label_text_day,.label_text_month,.label_text_year').addClass('color_red');
+  return false;
+} else {
+  $('.error_day,.error_month,.error_year').hide();
+  $('#dia,#mes,#ano').removeClass('border_color_red');
+  $('.label_text_day,.label_text_month,.label_text_year').removeClass('color_red');
+}
+
 
       return true;
     }
