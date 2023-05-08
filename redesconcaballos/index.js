4bdc8f313a1f74ca6c3be5cb293477e1cd5ebef5
almeida883 <almeidagamer99@gmail.com>
2023-05-08 11:35:26 +0100

    Update

--- redesconcaballos/index.js
+++ redesconcaballos/index.js
@@ -1,6 +1,8 @@
-const btnDropdown = document.querySelector('.btn-dropdown');
-const dropdownContent = document.querySelector('.dropdown-content');
+const btnDropdowns = document.querySelectorAll('.btn-dropdown');
+const dropdownContents = document.querySelectorAll('.dropdown-content');
 
-btnDropdown.addEventListener('click', function() {
-  dropdownContent.classList.toggle('show');
-});
+for (let i = 0; i < btnDropdowns.length; i++) {
+  btnDropdowns[i].addEventListener('click', function() {
+    dropdownContents[i].classList.toggle('show');
+  });
+}
